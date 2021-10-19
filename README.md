3c75976a2c113761901cab8951da4b621ee5d6d4
Daniel Hakim <daniel.hakim.jamaluddin@gmail.com>
2021-10-19 16:07:36 +0800

    Add bubble_sort.rb

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Ruby_Project-Bubble_Sort
 * Refresh back what I learned during my foundation study using C language.
 * It's gonna be simpler I think because Ruby has a lot of useful enumerable methods.
+---
+19/10/21:
+* There are many ways of swapping technique that can be done in Ruby. I choose the 'Ruby Magic'.
+* When I was learning C, the only way that possible to swap between two elements is by using temporary variable assignment.
+---
---
+++ bubble_sort.rb
@@ -0,0 +1,26 @@
+def bubble_sort(arr)
+
+  for i in 0...arr.length-1 do
+    for j in 1...arr.length do
+        arr[j],arr[j-1]=arr[j-1],arr[j] if(arr[j]<arr[j-1])
+    end
+  end
+  
+  arr
+end
+
+array  = [4,3,78,2,0,2]
+array2 = [20,13,2,33,12]
+p sorted=bubble_sort(array)
+p sorted=bubble_sort(array2)
+
+
+=begin #This the normal way of swapping element in an array.
+
+if(arr[j]<arr[j-1])
+  #temp   = arr[j]
+  #arr[j] = arr[j-1]
+  #arr[j-1] = temp
+end
+
+=end
